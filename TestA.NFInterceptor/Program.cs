0e4e69e8d83c24315835bcb887c345e674bdb1e5
Ivan Bezrodnyi <Ivan_Bezrodnyi@epam.com>
2021-02-19 14:24:49 +0300

    simple case of if/else statement is working

--- TestA.NFInterceptor/Program.cs
+++ TestA.NFInterceptor/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
 
 namespace TestA.Interceptor
 {
+    /* INFO *
+        https://cecilifier.me/ - online translator C# ti Mono.Cecil instruction on C#
+    */
+
     class Program
     {
         static void Main(string[] args)
@@ -23,23 +29,70 @@ namespace TestA.Interceptor
             ModuleDefinition module = ModuleDefinition.ReadModule(filename);
             MethodReference consoleWriteLine =
                 module.ImportReference(typeof(Console).GetMethod("WriteLine", new Type[] { typeof(object) }));
+            var call = Instruction.Create(OpCodes.Call, consoleWriteLine);
+            var nop = Instruction.Create(OpCodes.Nop);
 
             foreach (TypeDefinition type in module.Types)
             {
                 foreach (var methodDefinition in type.Methods)
                 {
+                    var methodName = methodDefinition.Name;
                     var ilBody = methodDefinition.Body;
                     var ilProcessor = ilBody.GetILProcessor();
+                    var instructions = ilBody.Instructions;
 
-                    var firstOp = methodDefinition.Body.Instructions.First();
-                    var ldstrEntering = Instruction.Create(OpCodes.Ldstr, $"--Entering {methodDefinition.Name}");
+                    //inject first instruction
+                    var firstOp = instructions.First();
+                    var ldstrEntering = Instruction.Create(OpCodes.Ldstr, $"\n--Entering {methodName}");
                     ilProcessor.InsertBefore(firstOp, ldstrEntering);
-                    var call = Instruction.Create(OpCodes.Call, consoleWriteLine);
                     ilProcessor.InsertBefore(firstOp, call);
-                    var ldstrLeaving = Instruction.Create(OpCodes.Ldstr, $"--Leaving {methodDefinition.Name}");
-                    var lastOp = methodDefinition.Body.Instructions.Last();
+
+                    //inject last instruction
+                    var ldstrLeaving = Instruction.Create(OpCodes.Ldstr, $"--Leaving {methodName}");
+                    var lastOp = instructions.Last();
                     ilProcessor.InsertBefore(lastOp, ldstrLeaving);
                     ilProcessor.InsertBefore(lastOp, call);
+
+                    //inject after return & throw
+                    //var ldstrBranch = Instruction.Create(OpCodes.Ldstr, $"-> BRANCH");                 
+                    Instruction ifInst = null;
+                    for (var i = 1; i < instructions.Count - 1; i++)
+                    {
+                        var op = instructions[i];
+                        var code = op.OpCode.Code;
+                        var flow = op.OpCode.FlowControl;
+
+                        //IF
+                        if (flow == FlowControl.Cond_Branch && (code == Code.Brfalse || code == Code.Brtrue || code == Code.Brfalse_S || code == Code.Brtrue_S))
+                        {
+                            ifInst = op;
+                            //when "after" set in desc order
+                            var ldstrIf = Instruction.Create(OpCodes.Ldstr, $"-> IF");
+                            ilProcessor.InsertAfter(op, call);
+                            ilProcessor.InsertAfter(op, ldstrIf);
+                            i += 2;
+                        }
+
+                        //ELSE
+                        if (code == Code.Br || code == Code.Br_S)
+                        {
+                            var op2 = instructions[i + 1];
+                            var elseInst = Instruction.Create(OpCodes.Ldstr, $"-> ELSE");
+
+                            ifInst.Operand = elseInst; //readdress 'else'
+                            ilProcessor.InsertBefore(op2, elseInst);
+                            ilProcessor.InsertBefore(op2, call);
+
+                            i += 2;
+                        }
+
+                        //if (/*op.OpCode.Code == Code.Leave_S || */flow == FlowControl.Return || flow == FlowControl.Throw)
+                        //{
+                        //    ilProcessor.InsertBefore(op, ldstrLeaving);
+                        //    ilProcessor.InsertBefore(op, call);
+                        //    i += 2;
+                        //}
+                    }
                 }
             }
 
@@ -49,5 +102,63 @@ namespace TestA.Interceptor
             Console.WriteLine($"Modified assembly is created: {modifiedPath}");
             Console.ReadKey(true);
         }
+
+        //http://www.swat4net.com/mono-cecil-part-ii-basic-operations/
+        //public static void ReweaveMethods(List<MethodDefinition> targetMethods)
+        //{
+        //    // we need to rescue the string.concat method which lives in mscorlib.dll
+        //    var assemblyMSCorlib = AssemblyDefinition.ReadAssembly(Console.Default.ILMsCorlibPath);
+
+        //    foreach (var method in targetMethods)
+        //    {
+        //        try
+        //        {
+        //            var methodToInject = assemblyMSCorlib.MainModule.Types.Where(o => o.IsClass == true)
+        //                                                                  .SelectMany(type => type.Methods)
+        //                                                                  .Where(o => o.FullName.Contains("Concat(System.String,System.String)")).FirstOrDefault();
+
+        //            var InstructionPattern = new Func<Instruction, bool>(x => (x.OpCode == OpCodes.Ldstr) && (x.Operand.ToString().Contains("Name")));
+        //            var InstructionPatternSet = new Func<Instruction, bool>(x => (x.OpCode == OpCodes.Callvirt) && (x.Operand.ToString().Contains("ClientLibrary.Entities.Person::set_Name(System.String)")));
+
+        //            if (null != methodToInject)
+        //            {
+        //                // importing aspect method 
+        //                var methodReferenced = method.DeclaringType.Module.ImportReference(methodToInject);
+        //                method.DeclaringType.Module.ImportReference(methodToInject.DeclaringType);
+
+        //                // defining ILProcesor
+        //                var processor = method.Body.GetILProcessor();
+
+        //                method.Body.SimplifyMacros();
+
+        //                // first update
+        //                var writeLines = processor.Body.Instructions.Where(InstructionPattern).ToArray();
+        //                foreach (var instruction in writeLines)
+        //                {
+        //                    var newInstruction = processor.Create(OpCodes.Ldstr, "Code Injected !");
+
+        //                    Instruction targetReferenceInstruction = processor.Create(OpCodes.Nop);
+        //                    targetReferenceInstruction = instruction.Previous;
+        //                    processor.InsertBefore(targetReferenceInstruction, newInstruction);
+        //                }
+
+        //                // second update
+        //                var writeLinesSet = processor.Body.Instructions.Where(InstructionPatternSet).ToArray();
+        //                foreach (var instruction in writeLinesSet)
+        //                {
+        //                    var concatInstruction = processor.Create(OpCodes.Call, methodReferenced);
+        //                    processor.InsertBefore(instruction, concatInstruction);
+        //                }
+
+        //                method.Body.OptimizeMacros();
+
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            //throw;
+        //        }
+        //    }
+        //}
     }
 }
--- TestNF/Program.cs
+++ TestNF/Program.cs
@@ -4,18 +4,18 @@ namespace TestNF
 {
     class Program
     {
-        delegate int Operation(int x, int y);
+        //delegate int Operation(int x, int y);
 
         static void Main(string[] args)
         {
-            //anonymous func is not crack the injection
-            int z = 8;
-            Operation operation = delegate (int x, int y)
-            {
-                return x + y + z;
-            };
-            int d = operation(4, 5);
-            Console.WriteLine($"Anonym func: {d}"); // 17
+            ////anonymous func
+            //int z = 8;
+            //Operation operation = delegate (int x, int y)
+            //{
+            //    return x + y + z;
+            //};
+            //int d = operation(4, 5);
+            //Console.WriteLine($"Anonym func: {d}"); // 17
 
             //FuncA
             try
@@ -25,13 +25,15 @@ namespace TestNF
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine($"\n{ex}");
             }
 
+            if (DateTime.Now.Second % 2 == 0)
+                Console.WriteLine("!!!");
+
             //FuncB
-            var r = new Random(DateTime.Now.Millisecond);
-                for (var i = 0; i < 5; i++)
-                    FuncB(r.Next(0, 2));
+            FuncB(0);
+            FuncB(1);
 
             Console.ReadKey(true);
         }
@@ -53,28 +55,21 @@ namespace TestNF
 
             //exception throw is not crack the injection
             if (isException)
+            {
+                Console.WriteLine("Will be throw!");
                 throw new Exception("Throw!");
-
-            Console.WriteLine("FunA: last instruction");
+            }
         }
 
         internal static void FuncB(int b)
         {
-            try
-            {
-                if (b % 2 == 0)
-                    Console.WriteLine("FunB: even");
-                else
-                    Console.WriteLine("FunB: odd");
-            }
-            //exception rethrow is not crack the injection
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                throw;
-            }
+            string type;
+            if (b % 2 == 0)
+                type = "even";
+            else
+                type = "odd";
 
-            Console.WriteLine("FunB: last instruction"); //for guaranteed injection for exit from the method (if this event is needed)
+            Console.WriteLine($"FunB: {type}");
         }
     }
 }
