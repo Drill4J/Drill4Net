74b6b744a8f32d0a10a34cf25e1c107bc7292095
Ivan Bezrodnyi <Ivan_Bezrodnyi@epam.com>
2021-05-10 14:24:20 +0300

    ...

---
+++ src/Tests/Demo/WebApp/Blazor/Drill4Net.Demo.Blazor.Server/Classes/MethodGroupType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drill4Net.Demo.Blazor.ServerApp
+{
+    public enum MethodGroupType
+    {
+        A = 0,
+        B = 1,
+        C = 3,
+        D = 4
+    }
+}
