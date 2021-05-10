b1ee381ce4d25c37b2997bc9ffabb701fe51335c
Ivan Bezrodnyi <Ivan_Bezrodnyi@epam.com>
2021-05-10 15:55:09 +0300

    ...

---
+++ src/Tests/Demo/Shared/Drill4Net.Demo.Target/src/CoverageTarget.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Drill4Net.Demo.Target
+{
+    public class CoverageTarget
+    {
+        public void IfElse_FullSimple_1(bool cond)
+        {
+            string type;
+            if (cond)
+                type = "yes";
+            else
+                type = "no";
+
+            Console.WriteLine($"{nameof(IfElse_FullSimple_1)}: {type}");
+        }
+
+        public void IfElse_FullSimple_2(bool cond)
+        {
+            string type;
+            if (cond)
+                type = "yes";
+            else
+                type = "no";
+
+            Console.WriteLine($"{nameof(IfElse_FullSimple_2)}: {type}");
+        }
+    }
+}
