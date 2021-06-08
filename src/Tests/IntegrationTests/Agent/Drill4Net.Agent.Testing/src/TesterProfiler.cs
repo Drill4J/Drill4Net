a57b953ded19d4d2321fbb5009fcb139a3d3594f
Ivan Bezrodnyi <Ivan_Bezrodnyi@epam.com>
2021-06-08 11:43:32 +0300

    start fix test Finalizer problem

--- src/Tests/IntegrationTests/Agent/Drill4Net.Agent.Testing/src/TesterProfiler.cs
+++ src/Tests/IntegrationTests/Agent/Drill4Net.Agent.Testing/src/TesterProfiler.cs
@@ -2,25 +2,27 @@
 using System.IO;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using NUnit.Framework.Internal;
 #if NETFRAMEWORK
 using System.Runtime.Remoting.Messaging;
 #endif
+using System.Linq;
+using System.Threading;
+using System.Reflection;
+using NUnit.Framework.Internal;
 using Serilog;
 using Drill4Net.Common;
 using Drill4Net.Agent.Abstract;
 using Drill4Net.Profiling.Tree;
-using System.Threading;
-using System.Reflection;
-using System.Linq;
 using Drill4Net.Agent.Testing.NetFxUtils;
 
 namespace Drill4Net.Agent.Testing
 {
     public class TesterProfiler : AbstractAgent
     {
+        private const string CONTEXT_UNKNOWN = "unknown";
         private static ConcurrentDictionary<string, Dictionary<string, List<string>>> _clientPoints;
         private static Dictionary<string, InjectedMethod> _pointToMethods;
+        private static Dictionary<int, string> _execIdToMethodOutput;
 
         /*****************************************************************************/
 
@@ -32,7 +34,7 @@ namespace Drill4Net.Agent.Testing
             try
             {
                 var domain = AppDomain.CurrentDomain;
-                _pointToMethods = domain.GetData("pointToMethods") as Dictionary<string, InjectedMethod>;
+                _pointToMethods = domain.GetData(nameof(_pointToMethods)) as Dictionary<string, InjectedMethod>;
                 if (_pointToMethods == null)
                 {
                     //rep
@@ -45,10 +47,13 @@ namespace Drill4Net.Agent.Testing
                     var treePath = Path.Combine(targetsDir, CoreConstants.TREE_FILE_NAME);
                     var tree = rep.ReadInjectedTree(treePath);
                     _pointToMethods = tree.MapPointToMethods();
-                    domain.SetData("pointToMethods", _pointToMethods);
+                    domain.SetData(nameof(_pointToMethods), _pointToMethods);
 
                     _clientPoints = new ConcurrentDictionary<string, Dictionary<string, List<string>>>();
-                    domain.SetData("clientPoints", _clientPoints);
+                    domain.SetData(nameof(_clientPoints), _clientPoints);
+
+                    _execIdToMethodOutput = new Dictionary<int, string>();
+                    domain.SetData(nameof(_execIdToMethodOutput), _execIdToMethodOutput);
 
                     Log.Debug("Initialized.");
                 }
@@ -67,7 +72,7 @@ namespace Drill4Net.Agent.Testing
             try
             {
                 #region Checks
-                var logCtx = GetLogicalContext();
+                var logCtx = GetContextId();
                 if (logCtx == null)
                     return;
 
@@ -105,46 +110,72 @@ namespace Drill4Net.Agent.Testing
             RegisterStatic(data);
         }
 
-        internal static TestExecutionContext GetLogicalContext()
+        internal static string GetContextId()
         {
 #if NETFRAMEWORK
             //What if NUnit or Tester Engine comes here?
-            var ret = LogicalContextManager.GetTestFromLogicalContext();
-            return ret;
+            var testCtx = LogicalContextManager.GetTestFromLogicalContext();
+            return GetContextId(testCtx);
 #else
-            //and for NetFx's tests, and for NetCore ones the Tester Engine will be on NetCore version
+            // the Tester Engine will be on NetCore version - and for NetFx's tests, and for NetCore ones 
             try
             {
-                //try load CallContext type - for NetFx successfully
-                var ret = LogicalContextManager.GetTestFromLogicalContext();
-                if (ret != null)
-                    return ret;
+                //try load old CallContext type - for NetFx successfully
+                var testCtx = LogicalContextManager.GetTestFromLogicalContext();
+                if (testCtx != null)
+                    return GetContextId(testCtx);
             }
             catch { } //it's normal under the NetCore
 
-            //...and for NetCore tests
-            var lstFld = typeof(ExecutionContext)
-                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                .FirstOrDefault(a => a.Name == "m_localValues");
+            //...and for NetCore tests NUnit uses AsyncLocal
+            var lstFld = Array.Find(typeof(ExecutionContext)
+                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance), a => a.Name == "m_localValues");
             if (lstFld != null)
             {
                 var lstFldVal = lstFld.GetValue(Thread.CurrentThread.ExecutionContext);
                 if (lstFldVal != null)
                 {
-                    //TODO: cache!!!
+                    //don't cache... (TODO: check it again)
                     var typeValMap = Type.GetType("System.Threading.AsyncLocalValueMap+ThreeElementAsyncLocalValueMap");
-                    var ctxFld = typeValMap
-                        .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                        .FirstOrDefault(a => a.Name == "_value3");
+                    var ctxFld = Array.Find(typeValMap
+                        .GetFields(BindingFlags.NonPublic | BindingFlags.Instance), a => a.Name == "_value3");
                     if (ctxFld != null)
                     {
-                        var testCtx = ctxFld.GetValue(lstFldVal) as TestExecutionContext;
-                        return testCtx;
+                        try
+                        {
+                            var testCtx = ctxFld.GetValue(lstFldVal) as TestExecutionContext;
+                            return GetContextId(testCtx);
+                        }
+                        catch 
+                        {
+                            //here we will be, for example, for object's Finalizers
+                            typeValMap = Type.GetType("System.Threading.AsyncLocalValueMap+OneElementAsyncLocalValueMap");
+                            ctxFld = Array.Find(typeValMap
+                                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance), a => a.Name == "_value1");
+                            //no context info about concrete test
+                            var testCtx = ctxFld.GetValue(lstFldVal) as TestExecutionContext;
+                            var testOutput = testCtx?.CurrentResult?.Output ?? CONTEXT_UNKNOWN;
+
+                            //This defines the logical execution path of function callers regardless
+                            //of whether threads are created in async/await or Parallel.For
+                            //It doesn't work very well on its own, at least not for everyone's version 
+                            //of the framework.
+                            var execId = Thread.CurrentThread.ExecutionContext.GetHashCode();
+                            if(_execIdToMethodOutput.ContainsKey(execId))
+                                _execIdToMethodOutput.Add(execId, testOutput);
+
+                            return GetContextId(testCtx);
+                        }
                     }
                 }
             }
-            return null;
+            return CONTEXT_UNKNOWN;
 #endif
+        }
+
+        internal static string GetContextId(TestExecutionContext ctx)
+        {
+            return ctx?.CurrentTest?.FullName ?? CONTEXT_UNKNOWN;
         }
 
         internal static void AddPoint(string asmName, string funcSig, string point)
@@ -186,34 +217,33 @@ namespace Drill4Net.Agent.Testing
             return all;
         }
 
+        /// <summary>
+        /// Gets list of the functions by current execution and logical contexts.
+        /// </summary>
+        /// <param name="createNotExistedBranch">If set to <c>true</c> create not existed branch for the current execution or logical context.</param>
+        /// <returns></returns>
         public static Dictionary<string, List<string>> GetFunctions(bool createNotExistedBranch)
         {
-            //This defines the logical execution path of function callers regardless
-            //of whether threads are created in async/await or Parallel.For
-            //var id = Thread.CurrentThread.ExecutionContext.GetHashCode();
             //Debug.WriteLine($"Profiler({createNotExistedBranch}): id={id}, trId={Thread.CurrentThread.ManagedThreadId}");
 
-            var logCtx = GetLogicalContext();
-            var method = logCtx?.CurrentTest?.FullName ?? "unknown";
+            var id = GetContextId();
 
-            if (_clientPoints == null)
-            {
-                if (AppDomain.CurrentDomain.GetData("clientPoints") is ConcurrentDictionary<string, Dictionary<string, List<string>>> clientPoints)
-                    _clientPoints = clientPoints;
-            }
+            if (_clientPoints == null && 
+                AppDomain.CurrentDomain.GetData(nameof(_clientPoints)) is ConcurrentDictionary<string, Dictionary<string, List<string>>> clientPoints)
+                _clientPoints = clientPoints;
             if (_clientPoints == null)
                 _clientPoints = new ConcurrentDictionary<string, Dictionary<string, List<string>>>();
             //
             Dictionary<string, List<string>> byFunctions;
-            if (_clientPoints.ContainsKey(method))
+            if (_clientPoints.ContainsKey(id))
             {
-                _clientPoints.TryGetValue(method, out byFunctions);
+                _clientPoints.TryGetValue(id, out byFunctions);
             }
             else
             {
                 byFunctions = new Dictionary<string, List<string>>();
                 if (createNotExistedBranch)
-                    _clientPoints.TryAdd(method, byFunctions);
+                    _clientPoints.TryAdd(id, byFunctions);
             }
             return byFunctions;
         }
@@ -222,7 +252,7 @@ namespace Drill4Net.Agent.Testing
         {
             if (_pointToMethods == null)
             {
-                if (AppDomain.CurrentDomain.GetData("pointToMethods") is Dictionary<string, InjectedMethod> pointToMethods)
+                if (AppDomain.CurrentDomain.GetData(nameof(_pointToMethods)) is Dictionary<string, InjectedMethod> pointToMethods)
                     _pointToMethods = pointToMethods;
             }
             if (_pointToMethods == null || !_pointToMethods.ContainsKey(probeUid))
