dfd335986be64eee273691738f83d23633798921
Ivan Bezrodnyi <Ivan_Bezrodnyi@epam.com>
2021-07-01 15:31:52 +0300

    fix hardcoded path to crosspoint's log in RnD profiler

--- src/RnD/Drill4Net.Agent.RnD/src/LoggerAgent.cs
+++ src/RnD/Drill4Net.Agent.RnD/src/LoggerAgent.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
-using System.Threading;
 
 namespace Drill4Net.Agent.RnD
 {
     //add this in project's csproj file: 
     //<CopyLocalLockFileAssemblies>true</CopyLocalLockFileAssemblies>
 
-    public class LoggerAgent
+    /// <summary>
+    /// Profiler for researches. Now just writes log to the file.
+    /// </summary>
+    public static class LoggerAgent
     {
         private static readonly string _filepath;
-        //private static readonly object _locker = new object();
-        //private static SpinLock sl = new SpinLock();
         private static readonly ChannelsQueue _queue;
 
         /*****************************************************************************/
 
         static LoggerAgent()
         {
-            //_filepath = Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location), "crosspoints.txt");
-
-            _filepath = @"d:\Projects\IHS-bdd.Injected\crosspoints.txt";
+            //D:\Projects\EPM-D4J\Drill4Net\build\bin\Debug\Drill4Net.Agent.RnD\netstandard2.0\crosspoints.txt - GetCallingAssembly
+            //d:\Projects\IHS-bdd.Injected\crosspoints.txt - GetEntryAssembly
+            _filepath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "crosspoints.txt");
             if (File.Exists(_filepath))
                 File.Delete(_filepath);
 
+            //we just write probes to the file
             Action<string> action = (string str) => File.AppendAllLines(_filepath, new string[] { str });
             _queue = new ChannelsQueue(action);
         }
@@ -33,26 +34,6 @@ namespace Drill4Net.Agent.RnD
 
         public static void RegisterStatic(string data)
         {
-            //we just write to the file
-
-            //var ar = data.Split('^'); //data can contains some additional info in the debug mode
-            //var probeUid = ar[0];
-            //var asmName = ar[1];
-            //var funcName = ar[2];
-            //var probe = ar[3];
-
-            //bool gotLock = false;
-            //try
-            //{
-            //    sl.Enter(ref gotLock);
-            //    File.AppendAllLines(_filepath, new string[] { data });
-            //}
-            //finally
-            //{
-            //    if (gotLock)`
-            //        sl.Exit();
-            //}
-
             _queue.Enqueue(data);
         }
     }
