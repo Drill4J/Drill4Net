56c49500e5607053d33bf008af6218f6ad73ac5f
Ivan Bezrodnyi <Ivan_Bezrodnyi@epam.com>
2021-07-05 20:04:21 +0300

    calling certains method with params in Standard Debug app

--- src/Agent/Drill4Net.Agent.Standard.Debug/src/Program.cs
+++ src/Agent/Drill4Net.Agent.Standard.Debug/src/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Drill4Net.Profiling.Tree;
@@ -8,7 +10,9 @@ namespace Drill4Net.Agent.Standard.Debug
 {
     class Program
     {
+        private static string _targetPath;
         private static int _pointRange = 200;
+        private static Dictionary<string, InjectedMethod> _methods;
         private static List<CrossPoint> _points;
         private const ConsoleColor COLOR_DEFAULT = ConsoleColor.Green;
 
@@ -34,7 +38,18 @@ namespace Drill4Net.Agent.Standard.Debug
                 var asmTree = tree.GetFrameworkRootDirectory(moniker);
                 if (asmTree == null)
                     throw new Exception($"Data for moniker {moniker} not found");
-                _points = asmTree.Filter(typeof(CrossPoint), true).Cast<CrossPoint>().ToList();
+                _points = asmTree.GetAllPoints().ToList();
+                var methList = asmTree.GetAllMethods().Where(a => !a.IsCompilerGenerated && a.Source.AccessType == AccessType.Public);
+                _methods = new Dictionary<string, InjectedMethod>();
+                foreach (var meth in methList)
+                {
+                    var key = meth.Name;
+                    if (!_methods.ContainsKey(key))
+                        _methods.Add(key, meth);
+                }
+
+                //TODO: from cfg!!!
+                _targetPath = @"d:\Projects\EPM-D4J\Drill4Net\build\bin\Debug\Tests\TargetApps.Injected\Drill4Net.Target.Net50.App\net5.0\";
 
                 ////debug
                 //var treeCnv = new TreeConverter();
@@ -43,7 +58,7 @@ namespace Drill4Net.Agent.Standard.Debug
 
                 //range
                 await Task.Delay(1500).ConfigureAwait(false); //wait for connect to admin side
-                WriteMessage($"\nCross points: {_points.Count}", ConsoleColor.Magenta);
+                WriteMessage($"\nCross points: {_points.Count}", ConsoleColor.Cyan);
                 WriteMessage($"Input point count for the extraction range [{_pointRange}]: ", ConsoleColor.Yellow);
                 var expr = Console.ReadLine()?.Trim();
                 if (int.TryParse(expr, out var pntCnt))
@@ -54,9 +69,9 @@ namespace Drill4Net.Agent.Standard.Debug
 
                 // info
                 const string mess = @"  *** Firstly, start session on admin side...
-  *** Press 1 for start some portion of target method's cross-points
-  *** Press q for exit
-  *** Good luck and... keep on dancing!";
+  *** Press 1 to send some portion of random target method's cross-points
+  *** Or input method name with arguments for real probe's executing, e.g. IfElse_Consec_Full true,false
+  *** Press q for exit";
                 WriteMessage($"\n{mess}", ConsoleColor.Yellow);
 
                 //polling
@@ -72,7 +87,7 @@ namespace Drill4Net.Agent.Standard.Debug
                     string output = null;
                     try
                     {
-                        StartMethods(expr);
+                        SendData(expr);
                     }
                     catch (Exception ex)
                     {
@@ -86,13 +101,28 @@ namespace Drill4Net.Agent.Standard.Debug
             {
                 Console.WriteLine(ex);
             }
-            Console.WriteLine("Done.");
+            WriteMessage("Done.", ConsoleColor.White);
         }
 
-        private static bool StartMethods(string input)
+        /// <summary>
+        /// Sends to the Admin side the points.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns></returns>
+        private static bool SendData(string input)
         {
-            if (input != "1")
-                return false;
+            if (input == "1")
+                return SendPointBlock();
+            else
+                return SendMethodData(input);
+        }
+
+        /// <summary>
+        /// Sends to the Admin side the portion of the points.
+        /// </summary>
+        /// <returns></returns>
+        private static bool SendPointBlock()
+        {
             if (_points.Count == 0)
             {
                 WriteMessage("No more points!", ConsoleColor.Red);
@@ -108,8 +138,98 @@ namespace Drill4Net.Agent.Standard.Debug
                 _points.RemoveAt(ind);
                 StandardAgent.RegisterStatic($"{point.PointUid}");
             }
-            WriteMessage($"Remaining points: {_points.Count}", ConsoleColor.Blue);
+            if (_points.Count == 0)
+                WriteMessage("No more points!", ConsoleColor.Red);
+            else
+                WriteMessage($"Remaining points: {_points.Count}", ConsoleColor.Blue);
             return true;
+        }
+
+        private static bool SendMethodData(string callData)
+        {
+            //get info
+            if (string.IsNullOrWhiteSpace(callData))
+            {
+                WriteMessage("No input", ConsoleColor.Red);
+                return false;
+            }
+            var ar = callData.Split(' ');
+            var name = ar[0];
+            if (!_methods.ContainsKey(name))
+            {
+                WriteMessage("No such method", ConsoleColor.Red);
+                return false;
+            }
+            var pars = new List<string>();
+            if (ar.Length > 1)
+            {
+                foreach (var par in ar[1].Split(','))
+                    if(!string.IsNullOrWhiteSpace(par))
+                        pars.Add(par.Trim());
+            }
+
+            //getting MethodInfo
+            var method = _methods[name];
+            MethodInfo methInfo;
+            object target;
+            try
+            {
+
+                WriteMessage($"Calling: {method.FullName}", ConsoleColor.White);
+                var asmPath = Path.Combine(_targetPath, method.AssemblyName);
+                var asm = Assembly.LoadFrom(asmPath);
+                var typeName = method.BusinessType;
+                var type = asm.GetType(typeName);
+                methInfo = type.GetMethod(name);
+                target = Activator.CreateInstance(asm.FullName, typeName).Unwrap();
+            }
+            catch (Exception ex)
+            {
+                WriteMessage($"Error of retrieving MethodInfo for [{method.FullName}] by reflection:\n{ex.Message}", ConsoleColor.Red);
+                return false;
+            }
+
+            //calling
+            try
+            {
+                var pars2 = ConvertToRealParameters(pars, method.Signature.Parameters);
+                methInfo.Invoke(target, pars2 );
+            }
+            catch (Exception ex)
+            {
+                WriteMessage($"Error of the method [{method.FullName}] calling: [{callData}]:\n{ex.Message}", ConsoleColor.Red);
+                return false;
+            }
+            return true;
+        }
+
+        private static object[] ConvertToRealParameters(List<string> parVals, string parTypes)
+        {
+            if (string.IsNullOrWhiteSpace(parTypes))
+                return null;
+            object[] res = new object[parVals.Count];
+            var types = parTypes.Split(',');
+            for (var i = 0; i < parVals.Count; i++)
+            {
+                object obj = null;
+                var val = parVals[i];
+                if (val != "null")
+                {
+                    var type = types[i];
+                    switch (type)
+                    {
+                        case "System.Boolean": obj = bool.Parse(val); break;
+                        case "System.Int32": obj = int.Parse(val); break;
+                        case "System.Object":
+                        case "System.String": obj = val; break;
+                        default:
+                            WriteMessage($"Unknown type of data: [{type}]", ConsoleColor.DarkYellow);
+                            break;
+                    }
+                }
+                res[i] = obj;
+            }
+            return res;
         }
 
         private static void WriteMessage(string mess, ConsoleColor color = COLOR_DEFAULT)
