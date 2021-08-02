7248928557791c2a3e47e494f3fb5b954d5a25f7
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-08-02 16:50:55 +0300

    serilog tests

--- src/Tests/Benchmark/Drill4Net.Benchmark.Logger/src/Tests.cs
+++ src/Tests/Benchmark/Drill4Net.Benchmark.Logger/src/Tests.cs
@@ -17,8 +17,13 @@ namespace Drill4Net.Benchmark.Logger
     public class Tests
     {
         private List<string> _testData;
+        private string _testString;
+        private int _amountOfStrings;
         private string _fileName="LogFile.txt";
-        private LoggerConfiguration _seriLog;
+        private string _fileNameSeriLog = "LogFileSerilog.txt";
+        private string _fileNameNLog = "LogFileNlog.txt";
+        private string _fileNameBanderLog = "LogFileBanderLog.txt";
+        //private Serilog.ILogger _seriLog;
         private class Config : ManualConfig
         {
 
@@ -30,15 +35,11 @@ namespace Drill4Net.Benchmark.Logger
         [GlobalSetup]
         public void GlobalSetup()
         {
-            _testData = Enumerable.Repeat(new string('a', 5000), 100000).ToList();
-            _fileName = @"C:\LogFile.txt";
-            Log.Logger=
-             new LoggerConfiguration()
-    .WriteTo.File("log.txt")
-    .CreateLogger();
-
-            log.Information("Hello, Serilog!");
-            //System.IO.File.Delete(fileName);
+            _testString = new string('a', 5000);
+            _testData = Enumerable.Repeat(_testString, _amountOfStrings).ToList();         
+            Log.Logger=new LoggerConfiguration()
+                .WriteTo.File(_fileNameSeriLog)
+                .CreateLogger();
         }
         [Benchmark]
         public void WriteLogToFile()
@@ -46,9 +47,27 @@ namespace Drill4Net.Benchmark.Logger
             File.AppendAllLines(_fileName, _testData);
         }
         [Benchmark]
+        public void WriteLogSerilogForEach()
+        {
+            for(var i=0;i< _amountOfStrings; i++)
+            {
+                Log.Logger.Information(_testString);
+            }
+        }
+        [Benchmark]
         public void WriteLogSerilog()
         {
-            var log = 
+            Log.Logger.Information("{_testData}", _testData);
+        }
+        [Benchmark]
+        public void WriteLogNLog()
+        {
+           
+        }
+        [GlobalCleanup]
+        public void GlobalCleanUp()
+        {
+            Log.CloseAndFlush();
         }
     }
 
