8ebc26dc0b95247124647a7d6b804766070e8a1a
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-08-10 15:41:02 +0300

    add separate class for  FileSink constants

---
+++ src/Core/Drill4Net.BanderLog/src/Sinks/File/FileSinkConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drill4Net.BanderLog.Sinks.File
+{
+      static class FileSinkConstants
+    {
+        internal const string NAME_DEFAULT = "log.txt";
+        internal const int LINES_TO_FLUSH_COUNTER=100;
+        internal const double  MAX_TIME_GAP_FOR_FLUSH = 1;
+    }
+}
