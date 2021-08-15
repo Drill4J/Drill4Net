c234860a2ffde1f712003c596e4356fe98b3ad41
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-08-15 17:15:35 +0300

    FsPickler cross-platform check

---
+++ src/Tests/Demo/Service/FsPickler.Deserializer.Checker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Drill4Net.Common;
+using Drill4Net.Profiling.Tree;
+
+namespace FsPickler.Deserializer.Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InjectedSolution tree = null;
+                var bytes2 = File.ReadAllBytes(@"C:\Docker\injected_rewrited.tree");
+                var bytes1 = File.ReadAllBytes(@"C:\Docker\injected.tree");
+                bool isEqual = Enumerable.SequenceEqual(bytes1, bytes2);
+                for (int i=0;i<bytes1.Length; i++)
+                {
+                    if (bytes1[i]!=bytes2[i])
+                        Console.WriteLine($"Difference in element {i}");
+                }
+                try
+                {
+                    tree = Serializer.FromArray<InjectedSolution>(bytes2);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Tree data not serialized: [C:\\Docker\\injected_rewrited.tree].\n{ex}");
+
+                }
+                Console.WriteLine($"Tree Description {tree?.Description}");
+                Console.WriteLine($"Tree Count {tree?.Count.ToString()}");
+                Console.WriteLine($"Tree Name {tree?.Name}");
+                Console.WriteLine($"Tree StartTime {tree?.StartTime.ToString()}");
+                Console.WriteLine($"Tree StartTime {tree?.StartTime.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Can't deserialize Tree data:  { ex}");
+            }
+        }
+    }
+}
---
+++ src/Tests/Demo/Service/FsPickler.Deserializer/Worker.cs
@@ -0,0 +1,74 @@
+using Drill4Net.Common;
+using Drill4Net.Profiling.Tree;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FsPickler.Deserializer
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+          
+                InjectedSolution tree=null;
+                try
+                {
+                    
+                    var bytes2 = File.ReadAllBytes(System.IO.Path.Combine("test_folder", "injected.tree"));
+                    try
+                    {
+                        tree = Serializer.FromArray<InjectedSolution>(bytes2);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError($"Tree data not serialized: [{System.IO.Path.Combine("test_folder", "injected.tree")}].\n{ex}", DateTimeOffset.Now);
+                        
+                    }
+                    _logger.LogInformation($"Tree Description {tree?.Description}", DateTimeOffset.Now);
+                    _logger.LogInformation($"Tree Count {tree?.Count.ToString()}", DateTimeOffset.Now);
+                    _logger.LogInformation($"Tree Name {tree.Name}", DateTimeOffset.Now);
+                    _logger.LogInformation($"Tree StartTime {tree.StartTime.ToString()}", DateTimeOffset.Now);
+                    _logger.LogInformation($"Tree FinishTime {tree.FinishTime.ToString()}", DateTimeOffset.Now);                
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Can't deserialize Tree data: [{System.IO.Path.Combine("test_folder", "injected.tree")}] { ex}", DateTimeOffset.Now);
+                }
+                try
+                {
+                    var data = Serializer.ToArray<InjectedSolution>(tree);
+                    File.WriteAllBytes(
+                        System.IO.Path.Combine("test_folder", "injected_rewrited.tree"), data);
+                }
+
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Can't serialize Tree data:  { ex}", DateTimeOffset.Now);
+                }
+                tree = null;
+                var bytes1 = File.ReadAllBytes(System.IO.Path.Combine("test_folder", "injected_rewrited.tree"));
+                try
+                {
+                    tree = Serializer.FromArray<InjectedSolution>(bytes1);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Tree data not serialized: [{System.IO.Path.Combine("test_folder", "injected_rewrited.tree")}].\n{ex}", DateTimeOffset.Now);
+
+                }
+        }
+    }
+}
