932c9df03e773eb1646be360fd4fdbcaea897701
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-08-31 09:41:00 +0300

    fix cleaning of obj folders

--- scripts/clean_obj.ps1
+++ scripts/clean_obj.ps1
@@ -5,7 +5,7 @@ Param (
 if ($path -eq $null -or $path -eq ""){
 $path=$PSScriptRoot
 }
-$path='C:\Users\Viktoria_Nikolaeva\Documents\sources\Drill4Net3\'
+
 Write-Host("path "+$path)
 
 Get-ChildItem $path -include obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
