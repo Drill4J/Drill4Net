143be91a7fdb453fab423f6104efe2a0ccb92f8a
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-09-02 10:21:19 +0300

    add summary

--- src/Injector/Drill4Net.Injector.App/src/Helpers/BenchmarkLog.cs
+++ src/Injector/Drill4Net.Injector.App/src/Helpers/BenchmarkLog.cs
@@ -3,8 +3,19 @@ using Drill4Net.Injector.App.Helpers.Interfaces;
 
 namespace Drill4Net.Injector.App.Helpers
 {
-   internal static class BenchmarkLog
+    /// <summary>
+    /// Helper for writing benchmark information to separate log
+    /// </summary>
+    internal static class BenchmarkLog
     {
+        /// <summary>
+        /// Write benchmark information to log
+        /// </summary>
+        /// <param name="logger">Logger for writing data</param>
+        /// <param name="gitBranch">Git branch name for benchmarked method</param>
+        ///<param name="gitCommit">Git commit for benchmarked method</param>
+        /// <param name="benchmarkData">Data collected by benchmark</param>
+        /// <returns></returns>
         internal static void WriteBenchmarkToLog(IBenchmarkLogger logger,string gitBranch, string gitCommit, string benchmarkData)
         {
             var msg = $"{DateTime.Now}|{gitBranch}|{gitCommit}|{benchmarkData}";
