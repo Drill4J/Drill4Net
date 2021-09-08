988bc60b9a94f4e988843a37fddd24847c1d2250
Ivan Bezrodnyi <Ivan_Bezrodnyi@epam.com>
2021-09-08 15:56:08 +0300

    fix Anchor point placement for injections

--- src/Injector/Strategies/Drill4Net.Injector.Strategies.Blocks/src/Handlers/AnchorHandler.cs
+++ src/Injector/Strategies/Drill4Net.Injector.Strategies.Blocks/src/Handlers/AnchorHandler.cs
@@ -27,63 +27,21 @@ namespace Drill4Net.Injector.Strategies.Blocks
 
         protected override bool IsCondition(MethodContext ctx)
         {
-            var instr = ctx.CurInstruction;
-            var flow = instr.OpCode.FlowControl;
-            //
-            if (!ctx.Anchors.Contains(instr))
-                return false;
-            if (ctx.CompilerInstructions.Contains(instr))
-                return false;
-            if (flow is not FlowControl.Next and not FlowControl.Call)
-                return false;
-            if (IsPreviousBad(instr))
-                return false;
-            if (ctx.Processed.Contains(instr)) //it can already be processed
-                return false;
-
-            //we need use only 'pure jumps', for example, 'goto' statement,
-            //not if/else or cycles jumps, there are special handlers for these purposes
-            if (_ignoreCycles)
-            {
-                //TODO: caching!!!
-                foreach (var cur in ctx.Jumpers)
-                {
-                    Instruction[] operands;
-                    if (cur.Operand is Instruction instruction)
-                        operands = new[] { instruction };
-                    else
-                        operands = (Instruction[])cur.Operand; //'switch' statement
-                    //
-                    foreach (var operand in operands)
-                    {
-                        var anchor = operand;
-                        if (ctx.StartingInjectInstructions.Contains(anchor) && ctx.ReplacedJumps.ContainsKey(anchor))
-                            anchor = ctx.ReplacedJumps[anchor];
-                        if (anchor != instr)
-                            continue;
-                        var jumpFlow = cur.OpCode.FlowControl;
-                        if (jumpFlow == FlowControl.Cond_Branch)
-                            return false;
-                        if (jumpFlow == FlowControl.Branch && cur.Previous is { OpCode: { FlowControl: FlowControl.Cond_Branch } })
-                            return false;
-                    }
-                }
-            }
-            //
-            return true;
+            return false;
         }
+
 
         protected override void PostprocessConcrete(MethodContext ctx)
         {
             //in fact, will process not processed instructions
             var ret = ctx.Instructions.Last();
             var instrs = ctx.BusinessInstructions
-                                    .Where(a => ctx.Anchors.Contains(a) &&
-                                                !ctx.Processed.Contains(a) &&
-                                                !ctx.ReplacedJumps.ContainsKey(a) &&
-                                                !IsPreviousBad(a) &&
-                                                a != ret
-                                           );
+                .Where(a => ctx.Anchors.Contains(a) &&
+                            !ctx.Processed.Contains(a) &&
+                            !ctx.ReplacedJumps.ContainsKey(a) &&
+                            !IsPreviousLeave(a) &&
+                            a != ret
+                        );
             foreach (var instr in instrs)
             {
                 var ind = ctx.Instructions.IndexOf(instr);
@@ -92,15 +50,49 @@ namespace Drill4Net.Injector.Strategies.Blocks
                 var prevInd = ctx.Instructions.IndexOf(prev);
                 if (!IsRealCondition(prevInd, ctx))
                     continue;
+                if (_ignoreCycles && !CheckOnIfElseProcessed(ctx, instr))
+                {
+                    continue;
+                }
                 //
                 ctx.SetPosition(ind);
                 ProcessInstruction(ctx);
             }
         }
 
-        private bool IsPreviousBad(Instruction instr)
+        //Guano: ?
+        private bool CheckOnIfElseProcessed(MethodContext ctx, Instruction instr)
         {
-            return instr.Previous is { OpCode: { Code: Code.Leave or Code.Leave_S or Code.Br or Code.Br_S } };
+            //TODO: caching!!!
+            foreach (var cur in ctx.Jumpers)
+            {
+                Instruction[] operands;
+                if (cur.Operand is Instruction instruction)
+                    operands = new[] { instruction };
+                else
+                    operands = (Instruction[])cur.Operand; //'switch' statement
+                //
+                foreach (var operand in operands)
+                {
+                    var anchor = operand;
+                    if (ctx.StartingInjectInstructions.Contains(anchor) && ctx.ReplacedJumps.ContainsKey(anchor))
+                        anchor = ctx.ReplacedJumps[anchor];
+                    if (anchor != instr)
+                        continue;
+                    //
+                    var jumpFlow = cur.OpCode.FlowControl;
+                    if (jumpFlow == FlowControl.Cond_Branch)
+                        return false;
+                    if (jumpFlow == FlowControl.Branch && cur.Previous is { OpCode: { FlowControl: FlowControl.Cond_Branch } })
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private bool IsPreviousLeave(Instruction instr)
+        {
+            return instr.Previous is { OpCode: { Code: Code.Leave or Code.Leave_S } };
         }
     }
 }
