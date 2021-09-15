600f11ddaa06dc3d00d0e3fdbe1cdb67ab556941
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-09-15 15:52:59 +0300

    add compressors

---
+++ Drill4Net.Compressor.Benchmarks/src/Compression/DeflateCompressor.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Drill4Net.Compressor.Benchmarks.Compression
+{
+    internal static class DeflateCompressor
+    {
+        internal static byte[] CompressSimpleData(byte[] data, CompressionLevel compressionLevel)
+        {
+
+            MemoryStream compressedData = new MemoryStream();
+            using (DeflateStream dstream = new DeflateStream(compressedData, compressionLevel))
+            {
+                dstream.Write(data, 0, data.Length);
+            }
+            return compressedData.ToArray();           
+        }
+
+    }
+}
---
+++ Drill4Net.Compressor.Benchmarks/src/Compression/LZ4Compressor.cs
@@ -0,0 +1,23 @@
+﻿using K4os.Compression.LZ4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill4Net.Compressor.Benchmarks.Compression
+{
+    public static class LZ4Compressor
+    {
+        public static byte[] Compress(byte[] data, LZ4Level level)
+        {
+            var buffer = new byte[LZ4Codec.MaximumOutputSize(data.Length)];
+            var encodedLength = LZ4Codec.Encode(data, 0, data.Length, buffer, 0, buffer.Length, level);
+            if (encodedLength < 0)
+                throw new InvalidOperationException("Compression failed");
+            var compressedData = new byte[encodedLength];
+            Array.Copy(buffer, 0, compressedData, 0, encodedLength);
+            return compressedData;
+        }
+    }
+}
