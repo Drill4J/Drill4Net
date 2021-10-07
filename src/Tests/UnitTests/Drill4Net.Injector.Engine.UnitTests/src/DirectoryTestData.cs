0df1e694f96b934ca30a978c8200dcbba1b06b90
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-10-08 00:40:30 +0300

    add tests for moniker

---
+++ src/Tests/UnitTests/Drill4Net.Injector.Engine.UnitTests/src/DirectoryTestData.cs
@@ -0,0 +1,38 @@
+﻿using Drill4Net.Injector.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill4Net.Injector.Engine.UnitTests
+{
+    public class DirectoryTestData
+    {
+        private SourceFilterOptions CreateSourceFilterOptions(List<string> directoriesInclude, List<string> directoriesExclude, List<string> foldersInclude, List<string> foldersExclude)
+        {
+
+            var opt= new SourceFilterOptions
+            {
+                Includes = new SourceFilterParams(),
+                Excludes = new SourceFilterParams()
+            };
+            opt.Includes.Directories
+        }
+        public static IEnumerable<object[]> NeedProcessDirectoryTrue
+        {
+            get
+            {
+                return new List<object[]>()
+                {
+                    new object[]
+                    {
+                    },
+                    new object[]
+                    {
+                    }
+                };
+            }
+        }
+    }
+}
