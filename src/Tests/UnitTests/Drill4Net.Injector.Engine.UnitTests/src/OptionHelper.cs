5e24dfa867ef152d1ba6f00844ef9fddba9a7f65
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-10-08 09:33:50 +0300

    work on IsNeedProcessDirectory tests

---
+++ src/Tests/UnitTests/Drill4Net.Injector.Engine.UnitTests/src/OptionHelper.cs
@@ -0,0 +1,30 @@
+﻿using Drill4Net.Injector.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill4Net.Injector.Engine.UnitTests
+{
+    internal  class OptionHelper
+    {
+        internal SourceFilterOptions CreateSourceFilterOptions(List<string> directoriesInclude, List<string> directoriesExclude, 
+            List<string> foldersInclude, List<string> foldersExclude, List<string> NamespacesInclude, List<string> NamespacesExclude)
+        {
+
+            var opt = new SourceFilterOptions
+            {
+                Includes = new SourceFilterParams(),
+                Excludes = new SourceFilterParams()
+            };
+            opt.Includes.Directories = directoriesInclude;
+            opt.Excludes.Directories = directoriesExclude;
+            opt.Includes.Folders = foldersInclude;
+            opt.Excludes.Folders = foldersExclude;
+            opt.Includes.Namespaces = NamespacesInclude;
+            opt.Excludes.Namespaces = NamespacesExclude;
+            return opt;
+        }
+    }
+}
