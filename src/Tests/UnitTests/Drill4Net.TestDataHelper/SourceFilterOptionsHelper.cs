8b18f34b40b2969c17af2c582603d89b6e75f5e3
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-10-11 10:09:37 +0300

    ref

--- src/Tests/UnitTests/Drill4Net.TestDataHelper/SourceFilterOptionsHelper.cs
+++ src/Tests/UnitTests/Drill4Net.TestDataHelper/SourceFilterOptionsHelper.cs
@@ -7,34 +7,22 @@ namespace Drill4Net.TestDataHelper
 {
     public class SourceFilterOptionsHelper
     {
-        public SourceFilterOptions CreateSourceFilterOptions(SourceFilterParams includes, SourceFilterParams excludes, List<string> directoriesInclude,
-    List<string> foldersInclude, List<string> filesInclude, List<string> namespacesInclude, List<string> classesInclude, List<string> attributesInclude,
-    List<string> directoriesExclude, List<string> foldersExclude, List<string> filesExclude, List<string> namespacesExclude, List<string> classesExclude,
-    List<string> attributesExclude)
+        public SourceFilterOptions CreateSourceFilterOptions(SourceFilterParams includes, SourceFilterParams excludes)
         {
             var options = new SourceFilterOptions
             {
                 Includes = includes,
                 Excludes = excludes
             };
-            if (options.Includes != null)
+            return options;
+        }
+        public SourceFilterOptions CreateSourceFilterOptions()
+        {
+            var options = new SourceFilterOptions
             {
-                options.Includes.Directories = directoriesInclude?.ToList();
-                options.Includes.Folders = foldersInclude?.ToList();
-                options.Includes.Files = filesInclude?.ToList();
-                options.Includes.Namespaces = namespacesInclude?.ToList();
-                options.Includes.Classes = classesInclude?.ToList();
-                options.Includes.Attributes = attributesInclude?.ToList();
-            }
-            if (options.Excludes != null)
-            {
-                options.Excludes.Directories = directoriesExclude?.ToList();
-                options.Excludes.Folders = foldersExclude?.ToList();
-                options.Excludes.Files = filesExclude?.ToList();
-                options.Excludes.Namespaces = namespacesExclude?.ToList();
-                options.Excludes.Classes = classesExclude?.ToList();
-                options.Excludes.Attributes = attributesExclude?.ToList();
-            }
+                Includes = new SourceFilterParams(),
+                Excludes = new SourceFilterParams()
+            };
             return options;
         }
     }
