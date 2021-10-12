57d64454cc502af7f60982a7a3c1130135ab7eeb
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-10-12 11:18:58 +0300

    work on tests+ref

--- src/Tests/UnitTests/Drill4Net.Common.UnitTests/src/CommonUtilsTests.cs
+++ src/Tests/UnitTests/Drill4Net.Common.UnitTests/src/CommonUtilsTests.cs
@@ -5,13 +5,10 @@ namespace Drill4Net.Common.UnitTests
 {
     public class CommonUtilsTests
     {
-        private const string module = "<Module>";
-
         #region DeconstructFullTypeName
         [Theory]
         [InlineData("Drill4Net.Target.Common.Another.AnotherTarget", "Drill4Net.Target.Common.Another", "AnotherTarget")]
-        [InlineData(module, null, module)]
-        [InlineData("", null, "")]
+        [InlineData("Drill4Net.Common", "Drill4Net", "Common")]
         public void Deconstruct_Full_TypeName_Test(string typeFullName, string expectedNs, string expectedType)
         {
             // Arrange       
@@ -22,11 +19,24 @@ namespace Drill4Net.Common.UnitTests
             Assert.Equal(expectedNs, result.ns);
             Assert.Equal(expectedType, result.type);
         }
+
+        [Theory]
+        [InlineData("<Module>")]
+        [InlineData("")]
+        public void Deconstruct_Full_TypeName_Null(string typeFullName)
+        {
+            // Arrange       
+            // Act
+            var result = CommonUtils.DeconstructFullTypeName(typeFullName);
+
+            // Assert
+            Assert.Null(result.ns);
+            Assert.Equal(typeFullName, result.type);
+        }
         #endregion
         #region GetRootNamespace
         [Theory]
         [InlineData("Drill4Net.Target.Common.Another.AnotherTarget", "Drill4Net")]
-        [InlineData(module,  null)]
         public void Get_Root_Namespace_Test(string typeFullName, string expectedResult)
         {
             // Arrange       
@@ -38,7 +48,20 @@ namespace Drill4Net.Common.UnitTests
         }
 
         [Theory]
+        [InlineData("Drill4Net")]
+        public void Get_Root_Namespace_Null(string typeFullName)
+        {
+            // Arrange       
+            // Act
+            var result = CommonUtils.GetRootNamespace(typeFullName);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Theory]
         [InlineData("")]
+        [InlineData(" ")]
         [InlineData(null)]
         public void Get_Root_Namespace_Throws_ArgumentNullException(string typeFullName)
         {
@@ -46,32 +69,46 @@ namespace Drill4Net.Common.UnitTests
             // Act
             // Assert
             Assert.Throws<ArgumentNullException>(() => CommonUtils.GetRootNamespace(typeFullName));
-
         }
         #endregion
         #region GetTypeByMethod
         [Theory]
-        [InlineData("Drill4Net.Target.Common.Another.AnotherTarget", "Drill4Net")]
-        [InlineData(module, null)]
+        [InlineData("System.Int32 Drill4Net.Target.Common.NotEmptyStringEnumerator::GetPosition()", "Drill4Net.Target.Common.NotEmptyStringEnumerator")]
+        [InlineData("System.Int32 Drill4Net.Target.Common.NotEmptyStringEnumerator:GetPosition()", "Drill4Net.Target.Common.NotEmptyStringEnumerator")]
+        [InlineData("System.Int32 Drill4Net.Target:Common.NotEmptyStringEnumerator::GetPosition()", "Drill4Net.Target")]
+        [InlineData(" :","")]
         public void Get_Type_By_Method_Test(string typeFullName, string expectedResult)
         {
             // Arrange       
-            // Act
-        
+            // Act    
+            var result = CommonUtils.GetTypeByMethod(typeFullName);
 
             // Assert
+            Assert.Equal(expectedResult, result);
+        }
 
+        [Theory]
+        [InlineData("System.Int32 GetPosition()")]
+        [InlineData("Drill4Net.Target.Common.NotEmptyStringEnumerator::GetPosition()")]
+        public void Get_Type_By_Method_Null(string typeFullName)
+        {
+            // Arrange       
+            // Act
+            var result = CommonUtils.GetTypeByMethod(typeFullName);
+
+            // Assert
+            Assert.Null(result);
         }
 
         [Theory]
         [InlineData("")]
         [InlineData(null)]
-        public void Get_Type_By_Method_Throws_ArgumentNullException(string typeFullName)
+        public void Get_Type_By_Method_Throws_ArgumentNullException(string methodFullName)
         {
             // Arrange       
             // Act
             // Assert
-
+            Assert.Throws<ArgumentNullException>(() => CommonUtils.GetTypeByMethod(methodFullName));
         }
         #endregion
     }
