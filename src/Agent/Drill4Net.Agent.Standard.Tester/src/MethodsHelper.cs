15821ddbf1b0a721c1678c2dc6577a2b54dd545a
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-10-13 16:44:51 +0300

    add MethodsHelper

---
+++ src/Agent/Drill4Net.Agent.Standard.Tester/src/MethodsHelper.cs
@@ -0,0 +1,28 @@
+﻿using Drill4Net.Profiling.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Drill4Net.Agent.Standard.Tester
+{
+    internal static class MethodsHelper
+    {
+        internal static List<InjectedMethod> GetSortedMethods(Dictionary<string, InjectedMethod> methods)
+        {
+            return methods.Values
+                .OrderBy(a => a.AssemblyName)
+                .ThenBy(a => a.BusinessType)
+                .ThenBy(a => a.Name) //more presentable than through FullName (due Return type, namespaces, etc it won't be alphabetical strict)
+                .ToList();
+        }
+
+        internal static Dictionary<int, InjectedMethod> GetMethodByOrderNumber(List<InjectedMethod> sorted)
+        {
+            var res = new Dictionary<int, InjectedMethod>();
+            for (var i = 0; i < sorted.Count; i++)
+                res.Add(i + 1, sorted[i]);
+            return res;
+        }
+    }
+}
