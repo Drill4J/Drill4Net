b45cec645fd28bcb6281c8e982a9dddb32477e36
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-10-13 22:19:36 +0300

    add TesterConstants and TesterOptionsHelper

---
+++ src/Agent/Drill4Net.Agent.Standard.Tester/src/TesterConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Drill4Net.Agent.Standard.Tester
+{
+    internal static class TesterConstants
+    {
+        internal const string CONFIG_NAME = "app.yml";
+        internal const string CSV_NAME = "tree.csv";
+        internal const ConsoleColor COLOR_INFO = ConsoleColor.Cyan;
+        internal const ConsoleColor COLOR_ERROR = ConsoleColor.Red;
+        internal const ConsoleColor COLOR_DEFAULT = ConsoleColor.Green;
+        internal const ConsoleColor COLOR_TEXT = ConsoleColor.White;
+        internal const ConsoleColor COLOR_TEXT_HIGHLITED = ConsoleColor.Yellow;
+    }
+}
---
+++ src/Agent/Drill4Net.Agent.Standard.Tester/src/TesterOptionsHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Drill4Net.Common;
+
+namespace Drill4Net.Agent.Standard.Tester
+{
+    internal static class TesterOptionsHelper
+    {
+        internal static TesterOptions GetOptions()
+        {
+            var cfgPath = Path.Combine(FileUtils.ExecutingDir, TesterConstants.CONFIG_NAME);
+            var deser = new YamlDotNet.Serialization.Deserializer();
+            return deser.Deserialize<TesterOptions>(File.ReadAllText(cfgPath));
+        }
+    }
+}
