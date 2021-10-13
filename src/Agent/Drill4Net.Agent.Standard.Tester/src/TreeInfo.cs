f3aa94f5a93f5269246fd48c7102814c2bef6408
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-10-13 16:24:11 +0300

    add TreeInfo

---
+++ src/Agent/Drill4Net.Agent.Standard.Tester/src/TreeInfo.cs
@@ -0,0 +1,21 @@
+﻿using Drill4Net.Profiling.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill4Net.Agent.Standard.Tester
+{
+    public class TreeInfo
+    {
+        public InjectedSolution injSolution;
+        public InjectedDirectory injDirectory;
+        public string targetPath;
+        public Dictionary<string, InjectedMethod> methods;
+        public TesterOptions opts;
+        public List<InjectedMethod> methodSorted;
+        public Dictionary<int, InjectedMethod> methodByOrderNumber;
+        public List<CrossPoint> points;
+    }
+}
