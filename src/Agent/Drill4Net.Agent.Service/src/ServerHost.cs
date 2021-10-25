f664cc6abc215015dbf8cee8d7c6d3b5c3ada746
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-10-25 17:56:04 +0300

    add comment for semVer

--- src/Agent/Drill4Net.Agent.Service/src/ServerHost.cs
+++ src/Agent/Drill4Net.Agent.Service/src/ServerHost.cs
@@ -10,6 +10,11 @@ using Drill4Net.Agent.Messaging.Transport.Kafka;
 
 //automatic version tagger including Git info
 //https://github.com/devlooped/GitInfo
+// semVer creates an automatic version number based on the combination of a SemVer-named tag/branches
+// the most common format is v0.0 (or just 0.0 is enough)
+// to change semVer it is nesseccary to create appropriate tag and push it to remote repository
+// patches'(commits) count starts with 0 again after new tag pushing
+
 [assembly: AssemblyInformationalVersion(
       ThisAssembly.Git.SemVer.Major + "." +
       ThisAssembly.Git.SemVer.Minor + "." +
