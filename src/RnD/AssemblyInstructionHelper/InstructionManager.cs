4f850519d02133a2d3dfdd3916acb43b6501cf69
Viktoria Nikolaeva <Viktoria_Nikolaeva@epam.com>
2021-11-10 00:04:25 +0300

    add try\catch blocks check

--- src/RnD/AssemblyInstructionHelper/InstructionManager.cs
+++ src/RnD/AssemblyInstructionHelper/InstructionManager.cs
@@ -10,11 +10,65 @@ namespace Drill4Net.AssemblyInstruction.Helper
     public class AsyncInstructionManager
     {
         public List<MethodInfo> AsyncMethodInfo { get; }
+
+        //**************************************************************************************//
+
         public AsyncInstructionManager(string assemmblyPath)
         {
             AsyncMethodInfo = new List<MethodInfo>();
             var methods = ReadAsyncMethodsFromAssembly(assemmblyPath);
             ProcessInstructions(methods);
+        }
+
+        //**************************************************************************************//
+
+        private int CalcStartIndex(MethodBody body)
+        {
+            var startIndex = 0;
+            while (startIndex < body.Instructions.Count())
+            {
+                if (IsNopeCheck(body.Instructions[startIndex]))
+                {
+                    startIndex++;
+                    break;
+                }
+                startIndex++;
+            }
+            return startIndex;
+        }
+
+        private void CheckStartTryBlock(MethodBody body, ref MethodInfo processedInfo, ref int currentInst)
+        {
+            var index= currentInst;
+            if (body.ExceptionHandlers.Any(_=>_.TryStart== body.Instructions[index]))
+            {
+                processedInfo.Instructions.Add(new InstructionData(body.Instructions[currentInst], false));
+                currentInst++;
+
+                while (currentInst < body.Instructions.Count && !IsNopeCheck(body.Instructions[currentInst]))
+                {
+                    processedInfo.Instructions.Add(new InstructionData(body.Instructions[currentInst], false));
+                    currentInst++;
+                }
+            }
+        }
+
+        private void CheckGenHandlerBlock(MethodBody body, ref MethodInfo processedInfo, ref int currentInst)
+        {
+            var index = currentInst;
+            if (body.ExceptionHandlers.LastOrDefault()?.HandlerStart == body.Instructions[index])
+            {
+                while (currentInst < body.Instructions.Count && body.ExceptionHandlers.LastOrDefault()?.HandlerEnd != body.Instructions[currentInst])
+                {
+                    processedInfo.Instructions.Add(new InstructionData(body.Instructions[currentInst], false));
+                    currentInst++;
+                }
+                while (currentInst < body.Instructions.Count && !IsNopeCheck(body.Instructions[currentInst]))
+                {
+                    processedInfo.Instructions.Add(new InstructionData(body.Instructions[currentInst], false));
+                    currentInst++;
+                }
+            }
         }
 
         private List<MethodDefinition> ReadAsyncMethodsFromAssembly(string assemmblyPath)
@@ -33,18 +87,11 @@ namespace Drill4Net.AssemblyInstruction.Helper
             foreach (var method in methods)
             {
                 Console.WriteLine($"Process \tType = {method.DeclaringType.Name}\n\t\tMethod = {method.Name}");
-                var startIndex = 0;
+
                 var methodInfo=(new MethodInfo(method.FullName));
-                while (startIndex < method.Body.Instructions.Count())
-                {
-                    if (method.Body.Instructions[startIndex].OpCode.Code == Code.Nop)
-                    {
-                        startIndex++;
-                        break;
-                    }
-                    startIndex++;
-                }
-                for (var i = 0; i < method.Body.Instructions.Count(); i++)
+                var startIndex = CalcStartIndex(method.Body);
+
+                for (var i = 0; i < method.Body.Instructions.Count; i++)
                 {
                     if (i < startIndex)
                     {
@@ -52,11 +99,14 @@ namespace Drill4Net.AssemblyInstruction.Helper
                     }
                     else
                     {
+                        CheckStartTryBlock(method.Body, ref methodInfo, ref i);
+                        CheckGenHandlerBlock(method.Body, ref methodInfo, ref i);
                         UserInstCheck(method.Body.Instructions.ToList(), ref methodInfo, ref i);
                     }
                 }
                 AsyncMethodInfo.Add(methodInfo);
                 var test = methodInfo.GetUserInstruction();
+                Console.WriteLine("Done.");
             }
         }
 
@@ -64,10 +114,12 @@ namespace Drill4Net.AssemblyInstruction.Helper
         {
             return inst.OpCode.Code == Code.Nop;
         }
+
         private bool IsLeaveCheck(Instruction inst)
         {
             return inst.OpCode.Code == Code.Leave || inst.OpCode.Code == Code.Leave_S;
         }
+
         private bool IsBrCheck(Instruction inst)
         {
             return inst.OpCode.Code == Code.Br || inst.OpCode.Code == Code.Br_S;
@@ -75,7 +127,7 @@ namespace Drill4Net.AssemblyInstruction.Helper
 
         public void UserInstCheck(List<Instruction> instList, ref MethodInfo processedInfo, ref int currentInst)
         {
-            //process nop
+            //process Nop
             if (IsNopeCheck(instList[currentInst]))
                 processedInfo.Instructions.Add(new InstructionData(instList[currentInst], false));
 
@@ -83,7 +135,7 @@ namespace Drill4Net.AssemblyInstruction.Helper
             else if (IsLeaveCheck(instList[currentInst]))
                 processedInfo.Instructions.Add(new InstructionData(instList[currentInst], false));
 
-            //process IsCompleted-GetResult branches
+            //process IsCompleted-GetResult blocks
             else if (instList[currentInst].Operand!=null && instList[currentInst].Operand.ToString().Contains("::get_IsCompleted()"))
             {
                 processedInfo.Instructions.Last().IsUserInst = false;
@@ -100,12 +152,11 @@ namespace Drill4Net.AssemblyInstruction.Helper
                     processedInfo.Instructions.Add(new InstructionData(instList[currentInst], false));
                     currentInst++;
                 }
+                currentInst--;
             }
 
             else
                 processedInfo.Instructions.Add(new InstructionData(instList[currentInst], true));
         }
-
-
     }
 }
