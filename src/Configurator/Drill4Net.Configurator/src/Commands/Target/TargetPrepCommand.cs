c2ca3055170e325bdc4b7983eb91143593a497d1
Ivan Bezrodnyi <79256699+epamX@users.noreply.github.com>
2022-01-09 11:14:41 +0300

    the first version of the command "trg prep"

--- src/Configurator/Drill4Net.Configurator/src/Commands/Target/TargetPrepCommand.cs
+++ src/Configurator/Drill4Net.Configurator/src/Commands/Target/TargetPrepCommand.cs
@@ -65,12 +65,55 @@ namespace Drill4Net.Configurator
 
         internal bool CheckInjectedTarget(string dir)
         {
+            string? err;
             if (!Directory.Exists(dir))
             {
-                var err = $"Directory does not exist: [{dir}]. Perform the target injection first by specified {CoreConstants.SUBSYSTEM_INJECTOR}'s config.";
+                err = $"Directory does not exist: [{dir}]. Perform the target injection first by specified {CoreConstants.SUBSYSTEM_INJECTOR}'s config.";
                 _logger.Error(err);
                 RaiseError(err);
                 return false;
+            }
+            //
+            var trCfgName = CoreConstants.CONFIG_NAME_DEFAULT; //Transmitter's config (it has default name)
+            var trCfgPath = Path.Combine(dir, trCfgName);
+            if (!File.Exists(trCfgPath))
+            {
+                var modelCfgPath = Path.Combine(_rep.GetInstallDirectory(), trCfgName);
+                if (!File.Exists(modelCfgPath))
+                {
+                    RaiseError($"Model {CoreConstants.SUBSYSTEM_TRANSMITTER}'s config not found: [{modelCfgPath}]");
+                    return false;
+                }
+                try
+                {
+                    //check cfg itself
+                    var opts = _rep.ReadAgentOptions(modelCfgPath);
+                    //perhaps later there will be additional configuration settings for a specific target
+                    _rep.WriteAgentOptions(opts, trCfgPath);
+                    RaiseMessage($"{CoreConstants.SUBSYSTEM_AGENT}'s config is written to the target directory: [{trCfgPath}]", CliMessageType.Info);
+                }
+                catch (Exception ex)
+                {
+                    err = $"Default agent's config exist in install directory but cannot be read: [{modelCfgPath}]";
+                    _logger?.Error(err, ex);
+                    RaiseError(err);
+                }
+            }
+            else
+            {
+                try
+                {
+                    //check cfg itself
+                    var opts = _rep.ReadAgentOptions(trCfgPath);
+                    //here we just check the fact of reading the config
+                    RaiseMessage($"{CoreConstants.SUBSYSTEM_AGENT}'s config is ok: [{trCfgPath}]", CliMessageType.Info);
+                }
+                catch (Exception ex)
+                {
+                    err = $"{CoreConstants.SUBSYSTEM_AGENT}'s config exist in target directory but cannot be read: [{trCfgPath}]";
+                    _logger?.Error(err, ex);
+                    RaiseError(err);
+                }
             }
 
             return true;
@@ -78,12 +121,19 @@ namespace Drill4Net.Configurator
 
         public override string GetShortDescription()
         {
-            return "<implementation in process...>";
+            return "Prepare the injected target to run it";
         }
 
         public override string GetHelp()
         {
-            return "The article has not been written yet";
+            return @$"Check and prepare the injected target for additional requirements, it is now the presence of an {CoreConstants.SUBSYSTEM_AGENT}'s config. If necessary, such a config is created using a model file, which, in turn, is configured by system settings.
+
+You can to do it by passing the short name of config file (local for Injector) or full path to it:
+    Example: trg prep -- cfg2
+    Example: trg prep -- ""d:\Targets\TargA.Injected\cfg2.yml""
+
+Another option is passing the injected target directory directly using ""dest_dir"" option:
+    Example: trg prep --dest_dir=""d:\Targets\TargA.Injected\""";
         }
     }
 }
