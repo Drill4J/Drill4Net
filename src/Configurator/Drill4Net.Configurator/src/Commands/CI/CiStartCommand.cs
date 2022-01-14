f07454127a2c08f8c8e77e4ccffa17e5f4773495
Ivan Bezrodnyi <79256699+epamX@users.noreply.github.com>
2022-01-14 11:28:51 +0300

    help article for "ci start" command was added

--- src/Configurator/Drill4Net.Configurator/src/Commands/CI/CiStartCommand.cs
+++ src/Configurator/Drill4Net.Configurator/src/Commands/CI/CiStartCommand.cs
@@ -127,7 +127,9 @@ namespace Drill4Net.Configurator
 
         public override string GetHelp()
         {
-            return "The article has not been written yet";
+            return @$"The command starts the CI pipeline: instrumentation by the {CoreConstants.SUBSYSTEM_INJECTOR} of one or more targets (SUT - system under test), and then launching automatic tests located in them using {CoreConstants.SUBSYSTEM_TEST_RUNNER}. The pipeline launch is currently integrated into the post-build event of .NET projects and is designed to facilitate development in the IDE. In the future, support for Jenkins, TeamCity, etc is planned.
+
+{HelpHelper.GetArgumentsForSourceConfig(CoreConstants.SUBSYSTEM_CI, RawContexts, "ci")}";
         }
     }
 }
