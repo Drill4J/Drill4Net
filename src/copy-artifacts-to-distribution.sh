07ef96e09f00858246eb14cfa6f18e3d5daf1492
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2022-02-23 13:51:08 +0600

    Sleep 15 second. Wait work script for copy artifacts to distribution

--- src/copy-artifacts-to-distribution.sh
+++ src/copy-artifacts-to-distribution.sh
@@ -5,11 +5,11 @@ ls --color=auto /distribution || true
 echo ""
 
 echo "list /app directory:"
-ls --color=auto ./app/ || true
+ls --color=auto /app/ || true
 echo ""
 
 echo "copy files and directories to /distribution:"
-cp -a ./app/* /distribution/
+cp -a /app/* /distribution/
 echo ""
 
 echo "Sleep 15 second. Wait work script for copy artifacts to distribution"
@@ -18,4 +18,8 @@ echo ""
 
 echo "list in directory /distribution:"
 ls --color=auto /distribution || true
+echo ""
+
+echo "Sleep 15 second. Wait work script for copy artifacts to distribution"
+sleep 15
 echo ""
