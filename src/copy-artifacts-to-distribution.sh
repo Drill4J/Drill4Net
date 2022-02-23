3f1419a144a0a426670a4d8f0745c2711f929017
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2022-02-23 12:59:37 +0600

    Change location copy-artifacts-to-distribution.sh

--- src/copy-artifacts-to-distribution.sh
+++ src/copy-artifacts-to-distribution.sh
@@ -4,6 +4,14 @@ echo "list in directory /distribution:"
 ls --color=auto /distribution || true
 echo ""
 
+echo "list /app directory:"
+ls --color=auto ./app/ || true
+echo ""
+
+echo "copy files and directories to /distribution:"
+cp -a ./app/* /distribution/
+echo ""
+
 echo "Sleep 15 second. Wait work script for copy artifacts to distribution"
 sleep 15
 echo ""
@@ -11,12 +19,3 @@ echo ""
 echo "list in directory /distribution:"
 ls --color=auto /distribution || true
 echo ""
-
-echo "list current directory:"
-ls --color=auto . || true
-echo ""
-
-echo "copy files and directories to /distribution:"
-cp -a . /distribution
-echo ""
-
